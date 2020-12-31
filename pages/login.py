5fcaa143eab61f6fa27886c2c1ae96a8fa976f2d
Amit Pathak <55128822+VikingPathak@users.noreply.github.com>
2020-12-31 13:55:35 +0530

    Removed unnecessary imports

--- pages/login.py
+++ pages/login.py
@@ -3,7 +3,6 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-import pandas as pd
 from app import app, mysql
 from flask import session
 import random
